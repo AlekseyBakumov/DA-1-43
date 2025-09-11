881fb583d5b92022be76d5457bbbd1a18cfc3126
BakumovAP <a.bakumov@g.nsu.ru>
2025-09-11 14:27:13 +0700

    Added more comments

--- DA-1-43.py
+++ DA-1-43.py
@@ -1,5 +1,26 @@
 import pandas as pd
 
+"""
+Description
+
+Two data frames are merged by common key (<id> in our case)
+
+Result will have data in columns from both frames
+
+Performed with inner join (by default, can be changed with parameter <how=>)
+
+With this method, result will consist of rows,
+that have their keys in both data frames
+e.g.
+    frame 1 keys = (1 2 3 4    )
+    frame 2 keys = (1   3   5 7)
+
+    result keys  = (1   3      )
+So, while two first frames have 4 rows each,
+result will consist of only 2 rows.
+"""
+
+# Utility, creates example data frame
 def create_data_frame1():
     data_frame = pd.DataFrame(
         {
@@ -13,6 +34,7 @@ def create_data_frame1():
     )
     return data_frame
 
+# Utility, creates example data frame
 def create_data_frame2():
     data_frame = pd.DataFrame(
         {
@@ -25,16 +47,21 @@ def create_data_frame2():
         )
     return data_frame
 
+# Two data frames, with common key "id"
 data_frame1 = create_data_frame1()
 data_frame2 = create_data_frame2()
+
 #print(data_frame1.head())
 print(f"Rows in data frame 1 = {len(data_frame1)}")
 #print(data_frame2.head())
 print(f"Rows in data frame 2 = {len(data_frame2)}")
 
+# Method of merge can be changed here (add how="<method>")
+# See pd.merge description to see available options 
 data_frame_merge = pd.merge(data_frame1,
                             data_frame2,
                             on="id")
 
+# Check result
 print(data_frame_merge.head(5))
 print(f"Rows in merged data frame = {len(data_frame_merge)}")
