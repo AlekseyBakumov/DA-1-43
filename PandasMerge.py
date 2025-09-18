a40aec7b0b58dbae696d874ebd1ad2cadd20bb3a
BakumovAP <a.bakumov@g.nsu.ru>
2025-09-18 12:47:38 +0700

    Changed names, added readme and created function for merge

---
+++ PandasMerge.py
@@ -0,0 +1,71 @@
+import pandas as pd
+
+# Utility, creates example data frame
+def __create_data_frame1__():
+    data_frame = pd.DataFrame(
+        {
+            "id": [0,    1,    2,    3   ],
+            "A":  ["A0", "A1", "A2", "A3"],
+            "B":  ["B0", "B1", "B2", "B3"],
+            "C":  ["C0", "C1", "C2", "C3"],
+            "D":  ["D0", "D1", "D2", "D3"],
+        },
+        index=[0, 1, 2, 3],
+    )
+    return data_frame
+
+# Utility, creates example data frame
+def __create_data_frame2__():
+    data_frame = pd.DataFrame(
+        {
+            "id": [0,    1,    2,    3   ],
+            "E":  ["E0", "E1", "E2", "E3"],
+            "D":  ["D0", "D1", "D2", "D3"],
+            "F":  ["F0", "F1", "F2", "F3"],
+        },
+        index=[0, 1, 2, 3],
+        )
+    return data_frame
+
+def pandas_merge(data_frame1, data_frame2, key_column, csv_output=False):
+    
+    if key_column not in data_frame1.columns:
+        raise ValueError(f"First data frame have no {key_column} column")
+    if key_column not in data_frame2.columns:
+        raise ValueError(f"Second data frame have no {key_column} column")
+    
+    
+    # Method of merge can be changed here (add how="<method>")
+    # See pd.merge description to see available options 
+    data_frame_merge = pd.merge(data_frame1,
+                                data_frame2,
+                                on=key_column)
+    
+    if csv_output:
+        data_frame_merge.to_csv('pandas_merge_output.csv', index=False)
+
+
+    return data_frame_merge
+
+
+# Two data frames, with common key "id"
+data_frame1 = __create_data_frame1__()
+data_frame2 = __create_data_frame2__()
+key_column = "id"
+
+# Usage example
+data_frame_merge = pandas_merge(data_frame1, 
+                                data_frame2, 
+                                key_column,
+                                csv_output=True)
+
+# Check result
+#print("Data frame 1:")
+#print(data_frame1.head())
+#print("Data frame 2:")
+#print(data_frame2.head())
+print("Merge result:")
+print(data_frame_merge.head(5))
+print(f"Rows in data frame 1 = {len(data_frame1)}")
+print(f"Rows in data frame 2 = {len(data_frame2)}")
+print(f"Rows in merged data frame = {len(data_frame_merge)}")
